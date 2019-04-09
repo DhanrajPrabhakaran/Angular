cd598f2ff25e3a72b8e92d6107dd33781d5994d6
Dhanraj Prabhakaran <dhanrajdhacks28@gmail.com>
2019-04-08 23:52:02 -0400

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,25 @@ import { MatDialogRef } from '@angular/material';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
+
 
   ],
   providers: [],
