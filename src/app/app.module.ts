0030249cc8b399e5e63de2e36a819ac9bab34439
Dhanraj Prabhakaran <dhanrajdhacks28@gmail.com>
2019-04-16 21:25:12 -0400

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +37,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
